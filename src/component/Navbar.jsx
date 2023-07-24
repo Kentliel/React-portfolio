f46053f17ac90997ce47c8ea93b118446ec684e7
Kentliel <kent.iluminati@gmail.com>
2023-07-24 15:46:20 +0200

    link kent home

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
   return (
     <div className='bg-gradient-to-r from-amber-800 via-indigo-950 to-stone-800 text-white flex justify-around items-center p-[10px] '>
-      <Link to="/K" className='font-Poppins mr-[613px] ml-[121px] flex items-center'><img className='w-[150px]' src='https://i.imgur.com/pJuDauk.png' alt='circuloLetra' />Kent</Link>
+      <Link to="/" className='font-Poppins mr-[613px] ml-[121px] flex items-center'><img className='w-[150px]' src='https://i.imgur.com/pJuDauk.png' alt='circuloLetra' />Kent</Link>
       <Link to="/" className='font-Poppins mr-10 p-2 hover:rounded hover:bg-zinc-700'>Home</Link>
       <Link to="/About" className='font-Poppins mr-10 p-2 hover:rounded hover:bg-zinc-700'>About</Link>
       <Link to="/Proyectos" className='font-Poppins mr-[70px] p-2 hover:rounded hover:bg-zinc-700'>Projects</Link>
